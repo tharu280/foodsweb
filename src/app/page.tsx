36433d8e46e7269574da2dbb7ba818cb75835c5a
Tharushika Abedheera <tharushika280@gmail.com>
2026-01-22 01:01:33 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from '@/lib/prisma'
 import { Menu } from '@/components/Menu'
 
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const foods = await prisma.foodItem.findMany({
     orderBy: { createdAt: 'desc' }
